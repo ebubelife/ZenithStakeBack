344d5f35976a5628d3624be549e1196cbd2a24c8
ebubelife <ebubeemeka19@gmail.com>
2024-02-02 10:40:11 +0100

    ...

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -73,8 +73,8 @@ class MembersController extends Controller
 
 
         $members = Members::where('is_vendor', false)
-        ->offset(5300) // Set the desired offset
-        ->limit(300) // Set the desired limit
+        ->offset(5500) // Set the desired offset
+        ->limit(200) // Set the desired limit
         ->get();
 
            $all_emails = array();
