926fe8b0911c773153cf6f9539199a971ab3feab
ebubelife <ebubeemeka19@gmail.com>
2024-02-27 22:43:19 +0100

    ...

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -530,7 +530,7 @@ class SalesController extends Controller
     $result = curl_exec($curl);
   
 
-   return response()->json(["message" => $result]);
+   return $result["data"];// response()->json(["message" => $result]);
 
 
 
