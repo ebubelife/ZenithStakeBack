ba73c7b8f83d29636961373cc588821b36fb37d7
ebubelife <ebubeemeka19@gmail.com>
2024-03-10 19:35:16 +0100

    ...

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -513,7 +513,7 @@ class SalesController extends Controller
         //
 
         $from = "2024-03-06";
-        $to = "2024-03-06";
+        $to = "2024-03-10";
 
         $total_sales = array();
 
@@ -521,7 +521,7 @@ class SalesController extends Controller
             
          
         curl_setopt_array($curl, [
-            CURLOPT_URL => 'https://api.flutterwave.com/v3/transactions?status=successful&from=2024-03-06&to=2024-03-08&page=1',
+            CURLOPT_URL => 'https://api.flutterwave.com/v3/transactions?status=successful&from=2024-03-06&to=2024-03-10&page=1',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CUSTOMREQUEST => 'GET',
            
@@ -545,7 +545,7 @@ $decoded_result = json_decode($firstResultBatch, true);
             
          
     curl_setopt_array($curl, [
-        CURLOPT_URL => 'https://api.flutterwave.com/v3/transactions?status=successful&from=2024-03-06&to=2024-03-08&page='.$i,
+        CURLOPT_URL => 'https://api.flutterwave.com/v3/transactions?status=successful&from=2024-03-06&to=2024-03-10&page='.$i,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_CUSTOMREQUEST => 'GET',
        
